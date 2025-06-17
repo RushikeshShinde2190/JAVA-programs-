2dfad90d022902628ce9d7997c0b63ca7c604db4
RushikeshShinde2190 <202401080048@mitaoe.ac.in>
2025-06-17 22:06:36 +0530

    Update and rename Instanceof to instanceDemo.java

--- instanceDemo.java
+++ instanceDemo.java
@@ -1,7 +1,9 @@
-public static void instanceDemo() {
+public class instanceDemo{
+public static void main(String[] args) {
         System.out.println("\n--- Instanceof ---");
         String str = "Anudip"; // non primitive data type 
         boolean result; // Verifies if it is an instance of String class 
         result = str instanceof String;
         System.out.println("Is str an instance of String? " + result);
     }
+}
